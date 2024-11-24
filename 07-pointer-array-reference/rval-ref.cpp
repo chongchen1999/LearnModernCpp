ce75e8891f39f8eab5aeaae790408b5df6bc73be
Chong Chen <chongchen1999@gmail.com>
2024-11-24 11:35:16 -0500

    update

--- 07-pointer-array-reference/rval-ref.cpp
+++ 07-pointer-array-reference/rval-ref.cpp
@@ -1,5 +1,14 @@
+#include <iostream>
 int main() {
+    std::cout << "S" << std::endl;
     int x = 10;
     int &y = x;
+    using ref2int = int &;
+    using refref2int = ref2int &;
+    refref2int r = x;
+
+    std::cout << x << ' ' << typeid(x).name() << std::endl;
+    std::cout << y << ' ' << typeid(y).name() << std::endl;
+    std::cout << r << ' ' << typeid(r).name() << std::endl;
     return 0;
 }
