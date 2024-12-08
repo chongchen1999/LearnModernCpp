9da859af5097c97550d31f31453d7b6425bf74ff
Chong Chen <chongchen1999@gmail.com>
2024-12-08 13:35:03 -0500

    update

---
+++ 14-namespaces/c_header.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>  // Include C standard I/O
+using std::printf;  // Bring only printf into the global namespace
+
+int main() {
+    printf("Hello, world!\n");  // Works fine now, using printf from std
+    return 0;
+}
