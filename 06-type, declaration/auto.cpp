3a1468e6de1a3cafe42f79ab46e894deb31d9686
Chong Chen <chongchen1999@gmail.com>
2024-12-08 12:02:16 -0500

    update

--- 06-type, declaration/auto.cpp
+++ 06-type, declaration/auto.cpp
@@ -6,8 +6,15 @@ void f(int d) {
     printf("%d\n", max);
 }
 
+void f(const int &a, const int &b) {
+
+}
+
 int main() {
-    auto x3 = {1,2,3};
+    const auto &x3 = {1,2,3};
+    const int x = 10;
+    const auto y = x;
+    f(x, y);
     std::cout << typeid(x3).name() << std::endl;
     return 0;
 }
