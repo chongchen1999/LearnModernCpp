a634b7b48f02b213a4da8fa3775ce1743a58eb1d
Chong Chen <chongchen1999@gmail.com>
2025-01-09 13:12:40 -0500

    after vacation

--- 04-the_standard_library/37-regular_expressions/regex_examples.cpp
+++ 04-the_standard_library/37-regular_expressions/regex_examples.cpp
@@ -1,46 +1,48 @@
+#include <fstream>
 #include <iostream>
 #include <regex>
 #include <string>
-#include <fstream>
 
 // Function to check if a string is an identifier
 bool is_identifier(const std::string& s) {
-    std::regex pat {"[_[:alpha:]]\\w*"};
+    std::regex pat{"[_[:alpha:]]\\w*"};
     return std::regex_match(s, pat);
 }
 
 // Function to search for misspellings of a name
 void search_misspellings() {
-    std::regex pat {"[Ss]tro?u?v?p?stra?o?u?p?b?"};
+    std::regex pat{"[Ss]tro?u?v?p?stra?o?u?p?b?"};
     std::smatch m;
-    for (std::string s; std::cin >> s; )
+    for (std::string s; std::cin >> s;)
         if (std::regex_search(s, m, pat))
-            if (m[0]!= "stroustrup" && m[0]!= "Stroustrup")
+            if (m[0] != "stroustrup" && m[0] != "Stroustrup")
                 std::cout << "Found: " << m[0] << '\n';
 }
 
 // Function to replace word - number pairs in a string
 void replace_word_number_pairs() {
-    std::string input {"x 1 y2 22 zaq 34567"};
-    std::regex pat {"(\\w+)\\s(\\d+)"};
-    std::string format {"{$1,$2}\n"};
+    std::string input{"x 1 y2 22 zaq 34567"};
+    std::regex pat{"(\\w+)\\s(\\d+)"};
+    std::string format{"{$1,$2}\n"};
     std::cout << std::regex_replace(input, pat, format);
 }
 
 // Function to output whitespace - separated words in a string
 void output_whitespace_separated_words() {
     std::string input = "aa as; asd ++eˆasdf asdfg";
-    std::regex pat {R"(\\s+(\\w+))"};
-    for (std::sregex_iterator p(input.begin(), input.end(), pat); p!= std::sregex_iterator{}; ++p)
+    std::regex pat{R"(\\s+(\\w+))"};
+    for (std::sregex_iterator p(input.begin(), input.end(), pat);
+         p != std::sregex_iterator{}; ++p)
         std::cout << (*p)[1] << '\n';
 }
 
 // Function to iterate over specific sub - matches
 void iterate_over_submatches() {
-    std::string input {"aa::bb cc::dd ee::ff"};
-    std::regex pat {R"((\\w+)([[:punct:]]+)(\\w+)\\s*)"};
-    std::sregex_token_iterator end {};
-    for (std::sregex_token_iterator p{input.begin(), input.end(), pat, {1, 3}}; p!= end; ++p)
+    std::string input{"aa::bb cc::dd ee::ff"};
+    std::regex pat{R"((\\w+)([[:punct:]]+)(\\w+)\\s*)"};
+    std::sregex_token_iterator end{};
+    for (std::sregex_token_iterator p{input.begin(), input.end(), pat, {1, 3}};
+         p != end; ++p)
         std::cout << *p << '\n';
 }
 
